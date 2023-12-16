da26c2b129d4ba4078e4814dda4c4d3a2a2392cb
Robin Rajesh <70363861+robz-bot@users.noreply.github.com>
2023-12-16 14:35:29 +0530

    Initial Commit

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+app.config['MONGO_URI'] =  "mongodb://localhost:27017/letstalk"
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+CORS(app)
 @app.route('/')
 def home():
     return 'Hello, World!'
@@ -9,3 +18,49 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/users', methods=['GET'])
+def get_registered_users():
+    return jsonify(mongo.db.users.find())
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    existing_user = mongo.db.users.find_one({'username': username})
+    if existing_user:
+        return jsonify({'message': 'Username already exists'}), 400
+
+    new_user = {'username': username, 'password': hashed_password}
+    result = mongo.db.users.insert_one(new_user)
+
+    user_id = result.inserted_id  # Get the inserted user's _id
+    token = create_access_token(identity=str(user_id))
+    return jsonify({'message': 'User registered successfully', 'token': token}), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = mongo.db.users.find_one({'username': username})
+    if user and bcrypt.check_password_hash(user['password'], password):
+        token = create_access_token(identity=str(user['_id']))
+        return jsonify({'message': 'Login successful', 'token': token}), 200
+    else:
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
